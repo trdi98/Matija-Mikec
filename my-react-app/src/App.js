5c10003501e14cb9a6db7e223b9ca82c4d403948
[trdi98] <[matija.mikec98@gmail.com]>
2024-05-02 18:36:18 +0200

    prve promjene

---
+++ my-react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './react_1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Moja prva aplikacija
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/randomStringGenerator.js
@@ -0,0 +1,6 @@
+const { generateRandomString } = require('random-string-generator');
+
+// Generate a random string of length 10
+const randomString = generateRandomString(10);
+console.log(randomString);
+
